b1ed47af376c383720ba3d86c3df27bb3242131a
yuji.yamada <yuji.yamada@toppan.co.jp>
2022-04-06 20:22:38 +0900

    add today

--- show.py
+++ show.py
@@ -6,4 +6,4 @@ import random
 with open("./hyakunin.txt", encoding="utf-8") as f:
     wakas = [s.strip() for s in f.readlines()]
 
-print(wakas[random.randrange(len(wakas))])
+print("¡“ú‚Ìˆê‹å" + wakas[random.randrange(len(wakas))])
