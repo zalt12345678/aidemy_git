4b03458c1c3a11ce695d7ea94778220ac53690c9
yuji.yamada <yuji.yamada@toppan.co.jp>
2022-04-06 20:21:38 +0900

    add main

--- show.py
+++ show.py
@@ -3,7 +3,11 @@
 
 import random
 
-with open("./hyakunin.txt", encoding="utf-8") as f:
-    wakas = [s.strip() for s in f.readlines()]
+def main():
 
-print(wakas[random.randrange(len(wakas))])
+    with open("./hyakunin.txt", encoding="utf-8") as f:
+        wakas = [s.strip() for s in f.readlines()]
+        print(wakas[random.randrange(len(wakas))])
+
+if __name__ == '__main__':
+    main()
